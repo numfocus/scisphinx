dd416e8a595b1a4a41b269060a4f37562bc2b6b8
Anthony Scopatz <scopatz@gmail.com>
2012-12-23 11:15:24 -0800

    setup.py now working

--- setup.py
+++ setup.py
@@ -9,30 +9,17 @@ from distutils.core import setup
 VERSION = "0.1"
 
 
-def update_submodule(subpack):
-    needs_init = 0 == len(os.listdir(subpack))
-    cmd = ['git', 'submodule', 'update', ]
-    if needs_init: 
-        cmd += ['--init', '--']
-    cmd += [subpack]
-    print " ".join(cmd)
-    rtn = subprocess.call(cmd)
-    return rtn
-
-
 def update_submodules():
-    rtn = subprocess.call(['git', 'submodule', 'sync', ])
-    rtn = subprocess.call(['git', 'submodule', 'update', ])
+    rtn  = subprocess.call(['git', 'submodule', 'sync'])
+    rtn += subprocess.call(['git', 'submodule', 'update'])
+    if 0 < rtn:
+        print("some submodules failed to sync or update, please check your "
+              "internet connection and try again later.")
     subpackages = {}
     totrtn = 0
     conf = ConfigObj('.gitmodules')
     for value in conf.values():
         subpack = value['path']
-        #rtn = update_submodule(subpack)
-        totrtn += rtn
-        if rtn != 0:
-            print("failed to update {0}, skipping.".format(subpack))
-            continue
         has_setup = 'setup.py' in os.listdir(subpack)
         if has_setup:
             print("setup.py found for {0}.".format(subpack))
@@ -41,22 +28,31 @@ def update_submodules():
         else:
             print("setup.py not found for {0}, copying wholesale.".format(subpack))
             subpackages[subpack] = (subpack, ['*'])
-    if 0 < totrtn:
-        print("some submodules failed to update or install, please check your "
-              "internet connection and try again later.")
     return subpackages
 
 
 def main():
     packages = ["scisphinx",]
-    package_dir = {"scisphinx": "scisphinx"},
-    package_data = {'scisphinx': ['*.js', '*.css', '*.json']},
+    package_dir = {"scisphinx": "scisphinx"}
+    package_data = {'scisphinx': ['*.js', '*.css', '*.json']}
 
+    initsadded = []
     subpackages = update_submodules()
     for subpack, (subdir, subdata) in sorted(subpackages.items()):
-        packages.append(subpack)
-        package_dir[subpack] = subdir
-        package_data[subpack] = subdata
+        for root, dirs, files in os.walk(subpack):
+            subname = root.replace(os.path.sep, '.')
+            packages.append(subname)
+            package_dir[subname] = root
+            #package_data[subname] = subdata
+            package_data[subname] = files
+            if '__init__.py' not in files:
+                initfile = os.path.join(root, '__init__.py')
+                with open(initfile, 'w'):
+                    pass
+                initsadded.append(initfile)
+            hiddendirs = [d for d in dirs if d.startswith('.')]
+            for d in hiddendirs:
+                dirs.remove(d)
 
     setupkw = dict(
         name="scisphinx",
@@ -70,7 +66,7 @@ def main():
                      "Environment :: Plugins",
                      "License :: OSI Approved :: BSD License",
                      "Topic :: Documentation"],
-        keywords="sphinx numpy ipython scipy breath C++",
+        keywords="sphinx numpy ipython scipy nbconvert breath C++",
         author="Anthony Scopatz, et al.",
         author_email="scopatz@gmail.com",
         url="http://github.com/numfocus/scisphinx",
@@ -78,7 +74,12 @@ def main():
         requires=["sphinx (>= 1.0.1)"],
         )
 
-    setup(**setupkw)
+    try:
+        setup(**setupkw)
+    finally:
+        for f in initsadded:
+            os.remove(f)
+
 
 if __name__ == '__main__':
     main()
